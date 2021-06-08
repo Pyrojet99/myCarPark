4d9550e1d6850692237a3ba12722267460c13801
Pyrojet99 <giacomo.cignoni@gmail.com>
2021-06-08 19:39:11 +0200

    Initial commit

---
+++ app/src/main/java/giacomo/cignoni/testandroid/mycarpark/MainActivity.java
@@ -0,0 +1,14 @@
+package giacomo.cignoni.testandroid.mycarpark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
